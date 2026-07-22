deca9d1f8f39c7236c05a5cbfbf0f8ea24583eed
troyw1987 <troygregorywilson1@gmail.com>
2026-07-22 10:11:22 -0700

    added render settings, optimized for preformance

---
+++ home/.config/hypr/settings/render.lua
@@ -0,0 +1,13 @@
+hl.config({
+	render = {
+		direct_scanout = 2,
+		new_render_scheduling = true,
+		use_fp16 = 0,
+		cm_enabled = false,
+		cm_auto_hdr = 0,
+		keep_unmodified_copy = 0,
+		use_shader_blur_blend = false,
+		ctm_animation = 0,
+		expand_undersized_textures = true,
+	},
+})
